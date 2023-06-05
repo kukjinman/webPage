1ac5b46a53438434ee965aa279c843712f895b77
kukjin <thater114@gmail.com>
2023-06-05 21:40:25 +0900

    calendar and todo list sync up done

--- js/calendar.js
+++ js/calendar.js
@@ -1,15 +1,17 @@
+import { setClickedDate } from "./todo.js";
 
-function calendar_init(){
+function calendar_init() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      selectable: true,
-      selectMirror: true,
+        initialView: 'dayGridMonth',
+        selectable: true,
+        selectMirror: true,
 
-      events: [],
-      dateClick: function(info) {
-        console.log('Clicked on: ' + info.dateStr);
-        // addEvent(info.date);
+        events: [],
+        dateClick: function (info) {
+            console.log('Clicked on: ' + info.dateStr);
+            // addEvent(info.date);
+            setClickedDate(info.dateStr);
         }
     });
 
@@ -18,8 +20,7 @@ function calendar_init(){
     calendar.render();
 }
 
-function addEvent(event)
-{
+function addEvent(event) {
     calendar.addEvent(event);
 }
 
--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,4 @@
 const todoForm = document.getElementById("todo-form");
-
 // todo-form의 id를 #로 표현
 // const todoInput = document.querySelector("#todo-form input");
 const todoInput = todoForm.querySelector("input");
@@ -7,14 +6,24 @@ const todoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 let toDos = [];
+let ClickedDate;
 
-function saveToDos(){
+function TodoList(date) {
+    this.date = date;
+    this.todos = [];
+}
+
+const todoLists = [];
+
+
+function saveToDos() {
     // localStorage.setItem("todos", toDos);
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
     // console.log("save" +  JSON.stringify(toDos));
+    localStorage.setItem("todoLists",JSON.stringify(todoLists));
 }
 
-function deleteToDo(event){
+function deleteToDo(event) {
 
     //event는 버튼 클릭하면 받는 default param이고 event.target은 버튼이 된다.
     //target.parentElement는 li로 우리가 없애고자 하는 list니까
@@ -30,22 +39,7 @@ function deleteToDo(event){
 }
 
 
-// function paintToDo(newTodo){
-//     const todo_cur_li = document.createElement("li");
-//     const todo_cur_span = document.createElement("span");
-//     const todo_cur_remove_btn = document.createElement("button");
-
-//     todo_cur_remove_btn.innerText="X";
-//     todo_cur_remove_btn.addEventListener("click",deleteToDo);
-
-//     todo_cur_span.innerText = newTodo;
-//     todo_cur_li.appendChild(todo_cur_span);
-//     todo_cur_li.appendChild(todo_cur_remove_btn);
-//     todoList.appendChild(todo_cur_li);
-
-// }
-
-function paintToDo(newTodo){
+function paintToDo(newTodo) {
     const todo_cur_li = document.createElement("li");
     todo_cur_li.id = newTodo.id;
     const todo_cur_span = document.createElement("span");
@@ -53,14 +47,30 @@ function paintToDo(newTodo){
 
     const todo_cur_remove_btn = document.createElement("button");
 
-    todo_cur_remove_btn.innerText="X";
-    todo_cur_remove_btn.addEventListener("click",deleteToDo);
+    todo_cur_remove_btn.innerText = "X";
+    todo_cur_remove_btn.addEventListener("click", deleteToDo);
 
     todo_cur_li.appendChild(todo_cur_span);
     todo_cur_li.appendChild(todo_cur_remove_btn);
     todoList.appendChild(todo_cur_li);
 
 }
+
+// 새로운 Todo 추가 함수
+function addNewTodo(date, newTodo) {
+    // 해당 날짜의 TodoList 찾기
+    let curTodoList = todoLists.find(list => list.date === date);
+
+    // 날짜별 TodoList가 없는 경우 새로 생성
+    if (!curTodoList) {
+        curTodoList = new TodoList(date);
+        todoLists.push(curTodoList);
+    }
+
+    // TodoList에 Todo 추가
+    curTodoList.todos.push(newTodo);
+}
+
 
 function handleToDoSummit(event) {
     event.preventDefault();
@@ -70,11 +80,13 @@ function handleToDoSummit(event) {
     todoInput.value = "";
 
     const newTodoObj = {
-        id: Date.now(), 
+        id: Date.now(),
         text: newTodo,
     };
 
     toDos.push(newTodoObj);
+    addNewTodo(ClickedDate,newTodoObj);
+
     paintToDo(newTodoObj);
     saveToDos();
 }
@@ -83,19 +95,22 @@ todoForm.addEventListener("submit", handleToDoSummit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if(savedToDos !== null)
-{
+if (savedToDos !== null) {
     console.log(savedToDos);
-    
+
     const parsedToDos = JSON.parse(savedToDos);
     // console.log(parsedToDos);
-    
+
     //화면이 refresh될떄마다 맨위에 줄에서 toDos가 []로 초기화 된다.
     //toDos는 누적되서 계속 todo를가지고 있는 array여서 기존에 있던 todo를
     //유지하고 싶으면 맨처음 기존 todo를 로드할때도 toDos를 업데이트 해줘야 한다.
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
-    
 
 
+
+}
+
+export function setClickedDate(date) {
+    ClickedDate = date;
 }
