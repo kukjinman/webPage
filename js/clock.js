16af086372236dfd8f10b31b177e3aaa5ebb1dd3
kukjin <thater114@gmail.com>
2023-07-26 18:40:05 +0900

    function name changed

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,13 @@
 const clock = document.querySelector("h2#clock");
 
-function getClock() {
+function updateClock() {
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2,"0");
-    const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
 
     clock.innerText = (`${hours} : ${minutes} : ${seconds}`);
 }
 
-getClock();
-setInterval(getClock, 1000);
+updateClock();
+setInterval(updateClock, 1000);
