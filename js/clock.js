4b63e20dc9165764bb9b69c41ab2bc10ad03cc1c
kukjin <thater114@gmail.com>
2023-09-27 14:28:08 +0900

    revert

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function updateClock() {
     const date = new Date();
-    const hours = String(date.getHours() + 6).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
 
